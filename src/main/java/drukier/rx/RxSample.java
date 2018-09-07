3ee1ce6347c3b49376c6a185fe1fc9fa2b64f024
cdruk <cdrukier@student.touro.edu>
2018-09-06 20:44:14 -0400

    rxjava

--- src/main/java/drukier/rx/RxSample.java
+++ src/main/java/drukier/rx/RxSample.java
@@ -1,4 +1,25 @@
 package drukier.rx;
 
+import com.google.common.util.concurrent.AbstractScheduledService;
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
 public class RxSample {
+
+    public static void main(String [] args){
+
+        Observable<Integer> observable = Observable.just(5, 4, 3, 2, 1);
+
+        Disposable disposable = observable
+                .filter(integer -> integer % 2 == 0)
+                .sorted()
+                .subscribeOn(Schedulers.computation())
+                .observeOn(Schedulers.single())
+                .subscribe(System.out::println);
+
+
+    }
 }
