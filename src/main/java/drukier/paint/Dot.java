95dd2802f78cc12ca4ff2f8b2b0a7dfffd07d3dd
cdruk <cdrukier@student.touro.edu>
2018-10-25 20:26:37 -0400

    drawing dots

---
+++ src/main/java/drukier/paint/Dot.java
@@ -0,0 +1,27 @@
+package drukier.paint;
+
+public class Dot {
+    private int dotX;
+    private int dotY;
+
+    public Dot(int dotX, int dotY) {
+        this.dotX = dotX;
+        this.dotY = dotY;
+    }
+
+    public int getDotX() {
+        return dotX;
+    }
+
+    public void setDotX(int dotX) {
+        this.dotX = dotX;
+    }
+
+    public int getDotY() {
+        return dotY;
+    }
+
+    public void setDotY(int dotY) {
+        this.dotY = dotY;
+    }
+}
