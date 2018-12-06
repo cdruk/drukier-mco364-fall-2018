d920d6a6b5ca58155d464cdeefc8e693b350c6eb
cdruk <cdrukier@student.touro.edu>
2018-12-06 18:13:15 -0500

    cleaned up rect

--- src/main/java/drukier/paint/FillRectangle.java
+++ src/main/java/drukier/paint/FillRectangle.java
@@ -8,10 +8,6 @@ public class FillRectangle extends Shape{
     private int y;
     private int endX;
     private int endY;
-    private int width;
-    private int height;
-    private int topLeftX;
-    private int topLeftY;
 
     public FillRectangle(int x, int y, Color color) {
         this.x = x;
@@ -21,46 +17,17 @@ public class FillRectangle extends Shape{
 
     public void setEndX(int endX) {
         this.endX = endX;
-        setWidth(Math.abs(endX - x));
-        setTopLeftX();
     }
 
     public void setEndY(int endY) {
         this.endY = endY;
-        setHeight(Math.abs(endY - y));
-        setTopLeftY();
     }
 
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    private void setTopLeftX() {
-        if (x < endX || x == endX) {
-            topLeftX = x;
-        } else {
-            topLeftX = endX;
-            endX = x;
-        }
-    }
-
-    private void setTopLeftY() {
-        if (y < endY || y == endY) {
-            topLeftY = y;
-        } else {
-            topLeftY = endY;
-            endY = y;
-        }
-    }
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        g.fillRect(topLeftX, topLeftY,
-                width, height);
+        g.fillRect(Math.min(x, endX), Math.min(y, endY),
+                Math.abs(endX - x), Math.abs(endY - y));
     }
 }
--- src/main/java/drukier/paint/Rectangle.java
+++ src/main/java/drukier/paint/Rectangle.java
@@ -8,11 +8,6 @@ public class Rectangle extends Shape {
     private int y;
     private int endX;
     private int endY;
-    private int width;
-    private int height;
-    private int topLeftX;
-    private int topLeftY;
-
 
     public Rectangle(int x, int y, Color color) {
         this.x = x;
@@ -22,46 +17,16 @@ public class Rectangle extends Shape {
 
     public void setEndX(int endX) {
         this.endX = endX;
-        setWidth(Math.abs(endX - x));
-        setTopLeftX();
     }
 
     public void setEndY(int endY) {
         this.endY = endY;
-        setHeight(Math.abs(endY - y));
-        setTopLeftY();
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    private void setTopLeftX() {
-        if (x < endX || x == endX) {
-            topLeftX = x;
-        } else {
-            topLeftX = endX;
-            endX = x;
-        }
-    }
-
-    private void setTopLeftY() {
-        if (y < endY || y == endY) {
-            topLeftY = y;
-        } else {
-            topLeftY = endY;
-            endY = y;
-        }
     }
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        g.drawRect(topLeftX, topLeftY,
-                width, height);
+        g.drawRect(Math.min(x, endX), Math.min(y, endY),
+                Math.abs(endX - x), Math.abs(endY - y));
     }
 }
